8937d3c4cc0361c9fc81c159cf95d86c64870f92
Amey Khairnar <ameykhairnar99@gmail.com>
2018-09-08 23:22:32 -0500

    bug_fix

---
+++ README.md
@@ -0,0 +1,24 @@
+# pymavs.github.io
+
+Python user group at UT Arlington
+
+Live page: Master Branch  
+Developer page: Dev Branch
+
+## Notes for Development
+
+Development page is being worked out on the 'Dev' branch.
+
+Add blog posts, video posts, event posts to the respective .js files in ./data like the templates. Use JSX instead of strings if you want.
+
+Because of ReactJS, you'll need to create a local server to perview the website during work.
+
+#### Frameworks being used
+- ReactJS
+- JQuery
+- Bootstrap
+
+Great opensource tools for development:
+- [Brackets IDE](http://brackets.io/)
+- [VS Code](https://code.visualstudio.com)
+- [Atom](https://atom.io)
--- data/blog.js
+++ data/blog.js
@@ -6,12 +6,10 @@ const blogs = [
     */
     [
         'Python is the best',
-        '23-12-3123',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis quam eros, eu euismod quam tincidunt fringilla. Nullam varius, lacus nec laoreet viverra, justo libero interdum eros, at iaculis enim augue sit amet tellus. Sed et consectetur ante, sit amet dignissim quam. Curabitur quis velit tellus. Mauris ut ultrices libero. Morbi venenatis augue id libero sodales molestie. In tristique congue pretium. Phasellus euismod nisl vel euismod convallis. Vivamus pharetra turpis dui, eu sodales sem pulvinar nec.'
-    ],
-    [
-        'Game of Thrones',
-        '23-32-1221',
-        'ransdgflasjdf\nkjalsdfjaksdf\nasdfadsfadsfadsf'
+        '09-08-2018',
+        <div>
+            Blogs will be posted here<br/><br/>
+            <img src="img/pymavsbanner.png" alt="banner" style={{"width": "100%"}}/>
+        </div>
     ]
 ]
