9bab58b2cea08d4020bcbd4d596b13faa5fab6f3
ij2872 <ijaimes2872@gmail.com>
2018-10-03 16:12:31 -0500

    Setting up webpack for the website

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.css',
+    entry: './src/index.scss',
     output: {
-        filename: 'index.css',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'assets'),
+        filename: 'dist/bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                "style-loader",
+                "css-loader", // translate CSS into common js
+                "sass-loader" // compiles sass to css
+                
+            }]
+        }]
     }
 };
 
