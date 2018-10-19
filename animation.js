0cbd7487d57c2a28eabbe92a8ccbb05d7457963a
18nguyenl <18nguyenl@gmail.com>
2018-10-19 14:14:25 -0500

    Fix vertical animation homepage

--- animation.js
+++ animation.js
@@ -1,8 +1,15 @@
 var timeline = new TimelineMax();
 
-
-timeline.from(".home-text", .7, {opacity: 0, x: "-200px", ease: Power2.easeOut});
-timeline.from(".banner-image", .5, {opacity: 0, ease: Power2.easeIn})
-timeline.from(".btn-red", .2, {opacity: 0, ease: Power2.easeIn});
-timeline.from(".btn-black", .2, {opacity: 0, ease: Power2.easeIn});
-timeline.from("nav", .7 , {opacity: 0, ease: Power2.easeInOut});
+if(window.innerHeight <  window.innerWidth){
+    timeline.from(".home-text", .7, {opacity: 0, x: "-200px", ease: Power2.easeOut});
+    timeline.from(".banner-image", .5, {opacity: 0, ease: Power2.easeIn})
+    timeline.from(".btn-red", .2, {opacity: 0, ease: Power2.easeIn});
+    timeline.from(".btn-black", .2, {opacity: 0, ease: Power2.easeIn});
+    timeline.from("nav", .7 , {opacity: 0, ease: Power2.easeInOut});
+} else {
+    timeline.from(".banner-image", .5, {opacity: 0, ease: Power2.easeIn})
+    timeline.from("nav", .7 , {opacity: 0, ease: Power2.easeInOut});
+    timeline.from(".home-text", .7, {opacity: 0, y: "-100px", ease: Power2.easeOut});
+    timeline.from(".btn-red", .2, {opacity: 0, ease: Power2.easeIn});
+    timeline.from(".btn-black", .2, {opacity: 0, ease: Power2.easeIn});
+}
